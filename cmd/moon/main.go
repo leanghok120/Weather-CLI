72a43768aa137f5412d96e3c470efaec993156e1
leanghok120 <leanghokoeng5@gmail.com>
2024-06-15 18:07:32 +0700

    Seperate displaying weather info

--- cmd/moon/main.go
+++ cmd/moon/main.go
@@ -1,19 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/leanghok120/moon/internal/weather"
-)
+import "github.com/leanghok120/moon/internal/weather"
 
 func main() {
-	location := weather.GetLocation()
-
-	weather := weather.GetWeather(location)
-
-	fmt.Printf("Location: %s\n", weather.Name)
-	fmt.Printf("Temperature: %.0f°C\n", weather.Main.Temp)
-	fmt.Printf("Humidity: %d\n", weather.Main.Humidity)
-	fmt.Printf("Description: %s\n", weather.Weather[0].Description)
-	fmt.Printf("Wind Speed: %.2f\n", weather.Wind.Speed)
+	weather.DisplayShort()
 }
---
+++ internal/weather/cmd.go
@@ -0,0 +1,16 @@
+package weather
+
+import (
+	"fmt"
+)
+
+func DisplayShort() {
+	location := GetLocation()
+	weather := GetWeather(location)
+
+	fmt.Printf("Location: %s\n", weather.Name)
+	fmt.Printf("Temperature: %.0f°C\n", weather.Main.Temp)
+	fmt.Printf("Humidity: %d\n", weather.Main.Humidity)
+	fmt.Printf("Description: %s\n", weather.Weather[0].Description)
+	fmt.Printf("Wind Speed: %.2f\n", weather.Wind.Speed)
+}
